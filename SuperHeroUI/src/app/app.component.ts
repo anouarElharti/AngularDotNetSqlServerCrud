f28f07b3fca9acec46f2656b4f5810f73bbe48e8
Elharti <AElharti@lims.com>
2023-06-01 15:29:42 +0000

    Adding the apis calls in angular, using services, eventemitter, @output, @input, two way binding

--- SuperHeroUI/src/app/app.component.ts
+++ SuperHeroUI/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { SuperHeroService } from './services/super-hero.service';
 export class AppComponent {
   title = 'SuperHeroUI';
   heroes: SuperHero[] = [];
+  heroToEdit?: SuperHero;
 
   constructor(private superHeroService: SuperHeroService) {}
 
@@ -17,5 +18,17 @@ export class AppComponent {
     this.superHeroService
       .getSuperHeroes()
       .subscribe((result: SuperHero[]) => (this.heroes = result));
+  }
+
+  initNewHero() {
+    this.heroToEdit = new SuperHero();
+  }
+
+  updateHeroList(heroes: SuperHero[]) {
+    this.heroes = heroes;
+  }
+
+  editHero(hero: SuperHero) {
+    this.heroToEdit = hero;
   }
 }
